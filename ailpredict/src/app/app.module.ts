cda258a27152725c160d5ba8c66197075f60bec1
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-11-04 10:00:08 +0300

    pushing all the app components

--- ailpredict/src/app/app.module.ts
+++ ailpredict/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { RegistrationpageComponent } from './registrationpage/registrationpage.component';
+import { NavbarpageComponent } from './navbarpage/navbarpage.component';
+import{FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginpageComponent,
+    RegistrationpageComponent,
+    NavbarpageComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ailpredict/src/app/navbarpage/navbarpage.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavbarpageComponent } from './navbarpage.component';
+
+describe('NavbarpageComponent', () => {
+  let component: NavbarpageComponent;
+  let fixture: ComponentFixture<NavbarpageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavbarpageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavbarpageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
