6cc9b66b8a86be73dd27267782b6dc5948ef1f13
Alfred Warui <alfredwarui17@gmail.com>
2021-11-04 13:42:24 +0300

    configure routes

--- ailpredict/src/app/app-routing.module.ts
+++ ailpredict/src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { RegistrationpageComponent } from './registrationpage/registrationpage.component';
 
 const routes: Routes = [
-
-  {}
+  {path:'',component:HomepageComponent},
+  {path:'register',component:RegistrationpageComponent},
+  {path:'login',component:LoginpageComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { 
-  
+export class AppRoutingModule {
+
 }
